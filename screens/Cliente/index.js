3888ce256f315e1ef6cdbcd73a7f62f157d57984
daajem <jimenezdaniel772@gmail.com>
2024-04-15 16:42:52 -0700

    15/06/2024

--- screens/Cliente/index.js
+++
@@ -1,8 +0,0 @@
-import HomeCliente from './HomeCliente';
-import LoginCliente from './LoginCliente';
-import ProfileCliente from './ProfileCliente';
-import PurchaseCliente from './PurchaseCliente';
-import RegisterCliente from './RegisterCliente';
-import ReporteAccidentes from './ReporteAccidentes';
-
-export { HomeCliente, LoginCliente, ProfileCliente, PurchaseCliente, RegisterCliente, ReporteAccidentes};
